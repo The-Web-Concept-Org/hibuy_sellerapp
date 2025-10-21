409ee946ad2c459285e799b030d29d1a7459fe3b
Asad-Faiz <Muhammadasadfaiz2000@gmail.com>
2025-10-21 11:05:30 +0500

    login url issue

--- lib/res/app_url/app_url.dart
+++ lib/res/app_url/app_url.dart
@@ -1,6 +1,6 @@
 class AppUrl {
   static const String baseUrl = 'https://dashboard.hibuyo.com/api';
-  static const String loginApi = '$baseUrl/sellerLogout';
+  static const String loginApi = '$baseUrl/sellerLogin';
   static const String logOutApi = '$baseUrl/sellerLogout';
   static const String registerApi = '$baseUrl/register';
   static const String kycAuthentication = '$baseUrl/kycAuthentication';
